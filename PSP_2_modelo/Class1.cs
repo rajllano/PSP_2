dcb3c40f4b19abbec028b79c98f6dc8f7f71086b
Max <Max@DESKTOP>
2017-04-10 21:16:40 -0500

    Proyecto inicial

---
+++ PSP_2_modelo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSP_2_modelo
+{
+    public class Class1
+    {
+    }
+}
